6fbb515e3036c72f12f9af3fb5c40a979b7b24b3
nico <nicolas.raoux@gmail.com>
2019-07-16 21:21:46 +0200

    cleaning code

--- app/src/main/java/com/example/checkpoint04/activities/DetailsActivity.java
+++ app/src/main/java/com/example/checkpoint04/activities/DetailsActivity.java
@@ -2,7 +2,6 @@ package com.example.checkpoint04.activities;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
